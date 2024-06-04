0e4cbe7b9865dc873b5c9de2c2c7710bff8e281e
Deepa Sarojam <153694311+deepasarojam-2024@users.noreply.github.com>
2024-06-03 21:30:54 -0400

    Update eva_recommender.py

--- Capstone-Project/eva_recommender.py
+++ Capstone-Project/eva_recommender.py
@@ -9,7 +9,6 @@ import numpy as np
 import pandas as pd
 import os
 import re
-from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster
 from branca.colormap import linear
 from wordcloud import WordCloud, STOPWORDS
